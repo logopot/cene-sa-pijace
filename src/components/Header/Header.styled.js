4e3fee68dc5331766982d259c3d2fdf6c4809dd1
Logopot <logopotposao@gmail.com>
2026-07-15 13:43:11 +0200

    Update Header.styled.js

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -1,27 +1,27 @@
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
-import { Navbar } from 'react-bootstrap'
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { Navbar } from "react-bootstrap";
 
 export const StyledNavbar = styled(Navbar)`
   background-color: ${({ theme }) => theme.colors.primaryGreen};
   padding: 0.75rem 1.5rem;
-`
+`;
 
 export const BrandLink = styled(Link)`
   display: flex;
   align-items: center;
-  gap: ${({ theme }) => theme.spacing.md};
+  gap: 0;
   text-decoration: none;
 
   &:hover {
     text-decoration: none;
   }
-`
+`;
 
 export const Brand = styled(Navbar.Brand)`
   color: ${({ theme }) => theme.colors.surface} !important;
   font-weight: 700;
-  font-size: 1.6rem;
+  font-size: 1.3rem;
   letter-spacing: 0.02em;
   margin: 0;
-`
+`;
