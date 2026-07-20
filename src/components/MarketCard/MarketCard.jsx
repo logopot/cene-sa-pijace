d466fdf54642d5b80e41b3e8357cf061c3687e27
Logopot <logopotposao@gmail.com>
2026-07-20 10:47:12 +0200

    Update MarketCard.jsx

--- src/components/MarketCard/MarketCard.jsx
+++ src/components/MarketCard/MarketCard.jsx
@@ -2,14 +2,14 @@ import { useTranslation } from 'react-i18next'
 import { LuStore } from 'react-icons/lu'
 import { Card } from 'react-bootstrap'
 import { translateDataValue } from '../../utils/translateValue.js'
-import { buildCityRoute } from '../../utils/market.js'
+import { buildMarketRoute } from '../../utils/market.js'
 import { CardLink, StyledCard, CardHeader, IconWrap, TitleWrap, CityName, MarketName } from './MarketCard.styled.js'
 
 function MarketCard({ grad, pijaca }) {
   const { t, i18n } = useTranslation()
 
   return (
-    <CardLink to={buildCityRoute(grad, i18n.language)}>
+    <CardLink to={buildMarketRoute(grad, pijaca, i18n.language)}>
       <StyledCard>
         <Card.Body>
           <CardHeader>
